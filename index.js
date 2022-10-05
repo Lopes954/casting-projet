d3ead476ceba2bf2ed1c4c48716ed4b17e2253d4
Yoh1 <87152153+Yoh1@users.noreply.github.com>
2022-10-05 12:18:47 +0200

    race

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+let tbody = document.querySelector('tbody');
 
 fetch('https://catfact.ninja/breeds?limit=10')
 .then(reponse => reponse.json())
 .then(reponse => {
 
-
             reponse.data.map(el => {
-
-                console.log(el.coat);
+                let tr = document.createElement('tr');
+                tbody.appendChild(tr)
+                let td = document.createElement('td');
+                td.setAttribute('class','race-tableau');
+                td.textContent = el.breed;
+                tr.appendChild(td)
             })
 })
