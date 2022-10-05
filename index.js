cbdc40748562976c5bfeb4dead848a4c00600ba9
Yoh1 <87152153+Yoh1@users.noreply.github.com>
2022-10-05 12:31:33 +0200

    select + race dans tableau

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 let tbody = document.querySelector('tbody');
+let petSelect = document.querySelector('select');
+
+console.log(petSelect);
+
+function getSelectionBreed(i){
+
+    petSelect.options;
+    petSelect.selectedIndex;
+
+return  petSelect.options[petSelect.selectedIndex].value   
+
+}
 
 fetch('https://catfact.ninja/breeds?limit=10')
 .then(reponse => reponse.json())
 .then(reponse => {
 
             reponse.data.map(el => {
+                let option = document.createElement('option');
+                option.setAttribute('value',el.breed)
+                option.textContent = el.breed;
+                petSelect.appendChild(option)
                 let tr = document.createElement('tr');
                 tbody.appendChild(tr)
                 let td = document.createElement('td');
